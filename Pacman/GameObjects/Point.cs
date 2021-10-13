0fed403ca8f95453fcdee86d78c1c99ed06b2d04
Aardhynlavneder <laveat1@student.op.ac.nz>
2021-10-14 11:49:52 +1300

    added point class which increment the score by 10

---
+++ Pacman/GameObjects/Point.cs
@@ -0,0 +1,38 @@
+﻿using FormsPixelGameEngine.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout.GameObjects
+{
+    class Point : TileObject
+    {
+        // CONSTANTS
+
+        private const int TEXTURE = 22;
+        private const int VALUE = 10;
+
+        // FIELDS
+        
+        private int value;
+
+        // CONSTRUCTOR
+
+        public Point(int index, World world)
+            : base(index, world, tileset.GetTileSourceRect(TEXTURE))
+        {
+            value = VALUE;
+        }
+
+        public override void Update()
+        {
+            if (game.PacManPosition.Equals(world.GetTile(this)))
+            {
+                game.Score += value;
+                world.QueueTileFree(this);
+            }
+        }
+    }
+}
